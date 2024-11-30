713d1a70bb01f04f7f13b5ba72a2245fff70ebe4
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-11-30 17:17:53 +0100

    [ADD] Backend connection setup

---
+++ app/src/main/java/com/team25/event/planner/core/Page.java
@@ -0,0 +1,35 @@
+package com.team25.event.planner.core;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Page<T> {
+    private List<T> content;
+    private Pageable pageable;
+    private boolean last;
+    private int totalPages;
+    private long totalElements;
+    private boolean first;
+    private int size;
+    private int number;
+    private Sort sort;
+    private int numberOfElements;
+    private boolean empty;
+
+    @Data
+    public static class Sort {
+        private boolean sorted;
+        private boolean empty;
+        private boolean unsorted;
+    }
+
+    @Data
+    public static class Pageable {
+        private int pageSize;   // Broj stavki po stranici
+        private int pageNumber; // Trenutna stranica
+        private boolean isUnpaged; // Da li je stranica bez paginacije
+    }
+
+}
---
+++ app/src/main/java/com/team25/event/planner/event/api/EventApi.java
@@ -0,0 +1,4 @@
+package com.team25.event.planner.event.api;
+
+public interface EventApi {
+}
