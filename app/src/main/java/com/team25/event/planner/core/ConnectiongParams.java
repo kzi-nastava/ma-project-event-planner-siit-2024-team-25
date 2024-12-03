7c1b35c072acffcbe29481678049f83312a0884c
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-12-03 12:21:43 +0100

    [CHANGE] BE ip address

--- app/src/main/java/com/team25/event/planner/core/ConnectiongParams.java
+++ app/src/main/java/com/team25/event/planner/core/ConnectiongParams.java
@@ -1,6 +1,9 @@
 package com.team25.event.planner.core;
 
-public class ConnectiongParams {
-    public static final String BASE_URL = "http://192.168.40.205:8080";
 
+
+import com.team25.event.planner.BuildConfig;
+
+public class ConnectiongParams {
+    public static final String BASE_URL = BuildConfig.BASE_URL;
 }
