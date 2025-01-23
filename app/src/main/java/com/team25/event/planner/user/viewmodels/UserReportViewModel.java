d00836f9985d33a7505322b8726134a6e2b022d8
Nikola Jolovic <nikolajolovic02@gmail.com>
2025-01-23 14:05:08 +0100

    [REFACTOR] Public profile fragment to open Report dialog

--- app/src/main/java/com/team25/event/planner/user/viewmodels/UserReportViewModel.java
+++ app/src/main/java/com/team25/event/planner/user/viewmodels/UserReportViewModel.java
@@ -1,8 +1,10 @@
 package com.team25.event.planner.user.viewmodels;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.team25.event.planner.core.ConnectionParams;
 import com.team25.event.planner.core.api.ResponseCallback;
@@ -10,13 +12,14 @@ import com.team25.event.planner.user.api.UserReportApi;
 import com.team25.event.planner.user.model.UserReportRequest;
 
 public class UserReportViewModel extends ViewModel {
+    private final UserReportApi _userReportApi = ConnectionParams.userReportApi;
 
-    private Long _reportedUserId;
-    public UserReportViewModel(Long reportedUserId){
+    private final Long _reportedUserId;
+
+    public UserReportViewModel(Long reportedUserId) {
         this._reportedUserId = reportedUserId;
         _currentReport.setValue(new UserReportRequest());
     }
-    private UserReportApi _userReportApi = ConnectionParams.userReportApi;
 
     private final MutableLiveData<Boolean> _isReportSend = new MutableLiveData<>(false);
     public LiveData<Boolean> isReportSend = _isReportSend;
@@ -26,7 +29,7 @@ public class UserReportViewModel extends ViewModel {
     private final MutableLiveData<String> _serverError = new MutableLiveData<>();
     public final LiveData<String> serverError = _serverError;
 
-    public void reportUser(){
+    public void reportUser() {
         this._currentReport.getValue().setReportedUserId(this._reportedUserId);
         _userReportApi.reportUser(this._currentReport.getValue()).enqueue(new ResponseCallback<>(
                 (report) -> {
@@ -35,4 +38,25 @@ public class UserReportViewModel extends ViewModel {
                 _serverError, "UserReportViewModel"));
     }
 
+    public void onReportDone() {
+        _isReportSend.postValue(false);
+    }
+
+    public static class Factory implements ViewModelProvider.Factory {
+        private final Long userId;
+
+        public Factory(Long userId) {
+            this.userId = userId;
+        }
+
+        @NonNull
+        @Override
+        @SuppressWarnings("unchecked")
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            if (modelClass.isAssignableFrom(UserReportViewModel.class)) {
+                return (T) new UserReportViewModel(userId);
+            }
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
 }
